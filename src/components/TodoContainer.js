a92ce1485bd9ad8c134690920d7f21f5d1fe4175
codecaiine <akabrouyannickn@gmail.com>
2021-09-29 13:43:31 +0000

    Create components files and TodoContainer

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TodoContainer = () => (
+  <div>
+    <h1>
+
+      Hello from Create React App
+
+    </h1>
+
+    <p>
+
+      I am in a React Component!
+
+    </p>
+
+  </div>
+);
+export default TodoContainer;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TodoContainer from './components/TodoContainer';
 
-ReactDOM.render(<App />,
-  document.getElementById('root'));
+ReactDOM.render(<TodoContainer />, document.getElementById('root'));
