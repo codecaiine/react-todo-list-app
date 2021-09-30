3d452234b7fae0d39a02cb5b543f051b228ca7f5
codecaiine <akabrouyannickn@gmail.com>
2021-09-30 00:35:48 +0000

    Add hamburger menu and style

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,47 +1,46 @@
 import React, { useState } from 'react';
 import { FaPlusCircle } from 'react-icons/fa';
-/* eslint-disable react/prop-types */
+import PropTypes from 'prop-types';
 
 const InputTodo = (props) => {
-        const [inputText, setInputText] = useState({
-            title: '',
-        });
+    const [title, setTitle] = useState('');
 
-        const onChange = (e) => {
-            setInputText({
-                ...inputText,
-                [e.target.name]: e.target.value,
-            });
-        };
+    const onChange = (e) => {
+        setTitle(e.target.value);
+    };
 
-        handleSubmit = (e) => {
-            const handleSubmit = (e) => {
-                e.preventDefault();
-                if (inputText.title.trim()) {
-                    props.addTodoProps(inputText.title);
-                    setInputText({
-                        title: '',
-                    });
-                }
-                return {};
-            };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (title.trim()) {
+            props.addTodoProps(title);
+            setTitle('');
+        }
+        return {};
+    };
 
-            return ( <
-                form onSubmit = { handleSubmit }
-                className = "form-container" >
-                <
-                input type = "text"
-                className = "input-text"
-                placeholder = "Add todo..."
-                value = { inputText.title }
-                name = "title"
-                onChange = { onChange }
-                /> < <
-                button type = "submit"
-                className = "input-submit"
-                aria - label = "Add" > < FaPlusCircle / > < /button> < /
-                form >
-            );
-        };
+    return ( <
+        form onSubmit = { handleSubmit }
+        className = "form-container" >
+        <
+        input type = "text"
+        className = "input-text"
+        placeholder = "Add todo..."
+        value = { title }
+        name = "title"
+        onChange = { onChange }
+        /> <
+        button type = "submit"
+        className = "input-submit"
+        aria - label = "Add" > < FaPlusCircle style = {
+            { color: 'darkcyan', fontSize: '20px', marginTop: '2px' } }
+        /></button >
+        <
+        /form>
+    );
+};
 
-        export default InputTodo;
+InputTodo.propTypes = {
+    addTodoProps: PropTypes.func.isRequired,
+};
+
+export default InputTodo;
